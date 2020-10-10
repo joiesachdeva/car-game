c1ad9b81615c62d8b926432f6876105c95b9d7b6
joiesachdeva <70386254+joiesachdeva@users.noreply.github.com>
2020-10-10 17:56:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas,back_img,playerCount;
+var gameState=0;
+var database;
+var form,player,game;
+
+function setup(){
+    createCanvas(500,500);
+
+    database=firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+    
+
+   
+}
+
+function draw(){
+    background("white");
+   
+    drawSprites();
+}
+
